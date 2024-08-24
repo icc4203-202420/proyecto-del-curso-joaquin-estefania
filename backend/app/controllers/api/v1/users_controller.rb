cbc460a6cc5b29d51817687aa6eff096cb41f2cc
Estefania Sandoval <easoto1@miuandes.cl>
2024-08-23 22:29:42 -0400

    Solicitud de revision entrega 1.1

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,45 +1,31 @@
 class API::V1::UsersController < ApplicationController
+  include Authenticable
+
   respond_to :json
-  before_action :set_user, only: [:show, :update]  
-  
-  def index
-    @users = User.includes(:reviews, :address).all   
+  before_action :set_user, only: [:friendships]
+  before_action :verify_jwt_token, only: [:friendships, :create_friendship]
+
+  # GET /api/v1/users/:id/friendships
+  def friendships
+    @friendships = @user.friendships.includes(:friend)
+    render json: @friendships.map { |f| { friend_id: f.friend.id, friend_name: f.friend.name } }, status: :ok
   end
 
-  def show
-  
-  end
+  # POST /api/v1/users/:id/friendships
+  def create_friendship
+    @friendship = @user.friendships.new(friend_id: params[:friend_id])
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user.id, status: :ok
+    if @friendship.save
+      render json: { message: 'Friendship created successfully.' }, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    #byebug
-    if @user.update(user_params)
-      render :show, status: :ok, location: api_v1_users_path(@user)
-    else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: @friendship.errors, status: :unprocessable_entity
     end
   end
 
   private
 
   def set_user
-    @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.fetch(:user, {}).
-        permit(:id, :first_name, :last_name, :email, :age,
-            { address_attributes: [:id, :line1, :line2, :city, :country, :country_id, 
-              country_attributes: [:id, :name]],
-              reviews_attributes: [:id, :text, :rating, :beer_id, :_destroy]
-            })
+    @user = User.find_by(id: params[:id])
+    render json: { error: 'User not found' }, status: :not_found unless @user
   end
 end
